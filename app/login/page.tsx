d4efa0031921751619ded57dd69a881d6c8dc2a5
Hi my name is MohammadHossein Vafaee <76791895+vafa2020@users.noreply.github.com>
2025-02-16 16:17:38 +0330

    validation with react-hook-form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,29 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
+type Inputs = {
+  email: string;
+  password: string;
+};
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <>
       <div className="max-w-sm bg-white shadow-xl rounded-xl mx-auto p-5">
         <h2 className="text-center text-gray-700 font-bold">Login</h2>
-        <form className="flex flex-col mx-auto gap-2">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col mx-auto gap-2"
+        >
           <div>
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -15,12 +32,25 @@ const Login = () => {
               Email
             </label>
             <input
-              type="email"
+              {...register("email", {
+                required: true,
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "email is not correct",
+                },
+              })}
+              type="text"
               name="email"
               id="email"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="email"
             />
+            {errors.email?.type === "required" && (
+              <span className="text-red-600 block">This field is required</span>
+            )}
+            {errors.email?.type === "pattern" && (
+              <span className="text-red-600">{errors.email.message}</span>
+            )}
           </div>
           <div>
             <label
@@ -30,17 +60,30 @@ const Login = () => {
               Password
             </label>
             <input
+              {...register("password", {
+                required: true,
+                minLength: {
+                  value: 5,
+                  message: "minLength character is 5",
+                },
+              })}
               type="password"
               name="password"
               id="password"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="password"
             />
+            {errors.password?.type === "required" && (
+              <span className="text-red-600 block">This field is required</span>
+            )}
+            {errors.password?.type === "minLength" && (
+              <span className="text-red-600">{errors.password.message}</span>
+            )}
           </div>
           <div className="flex items-center justify-between mt-3">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+              type="submit"
             >
               Login
             </button>
